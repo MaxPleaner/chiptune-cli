f02d23dbb84b505771b50cf28b28daa7647aed03
maxpleaner <maxpleaner@gmail.com>
2016-02-12 13:19:52 -0800

    work on polyrhythm generation

--- bloop_api.rb
+++ bloop_api.rb
@@ -2,30 +2,267 @@ require 'bloops' # the bloops o' phone
 require 'active_support/inflector'
 require 'byebug'
 
-class BloopApi
+class OddTimePhrase 
+  attr_accessor :odd_time_sequence, :min, :max
+  def self.random
+    time_pairs = [
+      [3, 32], [5, 32], [7, 32], [9, 32]
+    ]
+    return self.new(*time_pairs.sample)
+  end
+  def initialize(time1, time2)
+    @max, @min = [time1, time2].max, [time1, time2].min
+    @odd_time_sequence = [
+      Array.new((max / min), min), # play min N times
+      (max % min)                  # the leftover amount 
+    ].flatten
+  end
+end
 
+class BloopApi
+  # usage:
+    # Class methods:
+      # BloopApi.flow -------------------- Play random beats
+      # BloopApi.sick_beat --------------- Play sick beat
+      # BloopApi.odd_time ---------------- Generate a polythythm
+      # BloopApi.odd_rhythm_riff --------- Rhythm riff of (n) length, supports: [3, 5, 6, 7, 9]
+      # BloopApi.odd_melody_riff --------- Melody riff of (n) length, supports: [3, 5, 6, 7, 9]
+      # BloopApi.standard_rhythm_riff ---- Rhythm riff of (n) length, supports multiples of 4
+      # BloopApi.standard_melody_riff ---- Melody riff of (n) length, supports multiples of 4
+    # Instance methods:
+      # BloopApi.new.play_sick_beat ------ Play sick beat base
+      #              generate_riff ------- Play riff
+      #              new_melody_sound ---- Random melody instrument
+      #              new_rhythm_sound ---- Random rhythm instrument
+      #              rhythms ------------- Rhythm riffs
+      #              melodies ------------ Melody riffs
+      #              bass_tone ----------- Bass tone instrument
+      #              bass_drum ----------- Bass drum instrument
+      #              snare_drum ---------- Snare drum instrument
+      #              chord_tone ---------- Chord tone instrument
+      #              hand_drum ----------- Hand drum instrument
+
+  attr_reader :bloops
   def initialize(options={})
+    # create a Bloop instance
+    # options: tempo
     options = {
-      tempo: 320,
-    }.merge(options)
+      tempo: 120,
+    }.merge(options.reject { |k, v| v.nil?})
     @bloops = Bloops.new
     @bloops.tempo = options[:tempo]
   end
 
+  def self.odd_rhythm_riff(num_beats, mixed=false, idx=nil)
+    num_beats = num_beats.to_s
+    riffs = {
+      "2" => [
+       "16A 16C" 
+      ],
+      "4" => [
+        "16A 16C 16C 16C"
+      ],
+      "3" => [
+        "16A 16C 16C",
+      ],
+      "5" => [
+        "16A 16C 16C 16C 16C",
+      ],
+      "6" => [
+        "16A 16C 16C 16C 16C 16C"
+      ],
+      "7" => [
+        "16A 16C 16C 16C 16C 16C 16C",
+      ],
+      "8" => [
+        "16A 16C 16C 16C 16C 16C 16C 16C"
+      ],
+      "9" => [
+        "16A 16C 16C 16C 16C 16C 16C 16C 16C",
+
+      ]
+    }
+    begin
+    return mixed ? riffs[num_beats].sample : riffs[num_beats][idx || 0]
+  rescue StandardError; byebug; end
+  end
+
+  def self.odd_melody_riff(num_beats, mixed=false, idx=nil)
+    num_beats = num_beats.to_s
+    riffs = {
+      "2" => [
+       "-- 16A# 16B#" 
+      ],
+      "4" => [
+        "-- 16A# 16B# 16C# 16D#"
+      ],
+      "3" => [
+        "-- 16A# 16B# 16C#",
+      ],
+      "5" => [
+        "-- 16A# 16B# 16C# 16D# 16F#",
+      ],
+      "6" => [
+        "-- 16A# 16B# 16C# 16D# 16F# + 16A#"
+      ],
+      "7" => [
+        "-- 16A# 16B# 16C# 16D# 16F# + 16A# 16B#",
+      ],
+      "8" => [
+        "-- 16A# 16B# 16C# 16D# 16F# + 16A# 16B# 16C#"
+      ],
+      "9" => [
+        "-- 16A# 16B# 16C# 16D# 16F# + 16A# 16B# 16C# 16D#",
+      ]
+    }
+    (return mixed ? riffs[num_beats].sample : riffs[num_beats][idx || 0]) rescue byebug
+  end
+
+  def self.standard_rhythm_riff(length, mixed=false, idx=nil)
+    # combines 8 note riffs to fit the desired result.
+    # enter 'mixed' option to switch them up
+    riffs = [
+      " 16D 16 16C 16 16D 16 16C 16",
+      "16D 16 16 16 16D 16 16 16 ",
+      " 16 16 16 16 16D 16 16 16"
+    ]
+    res = []
+    until (res.length * 8) == length
+      res << mixed ? riffs.sample : riffs[idx || 0]
+    end
+    return res.join(" ")
+  end
+
+  def self.standard_melody_riff(length, mixed=false, idx=nil)
+    # combines 8 note riffs to fit the desired result.
+    # enter 'mixed' option to switch them up
+    riffs = [
+      "16B 16A 16A 16A 16B 16A 16A 16A",
+      "16 16 16 16A 16A 16B 16B 16B",
+      "16 16 16 16A 16A 16 16B 16B",
+    ]
+    res = []
+    until (res.length * 8) == length
+      res << mixed ? riffs.sample : riffs[idx || 0]
+    end
+    return res.join(" ")
+  end
+
+  # options: tempo, beat_count, repetitions, solo
+  # tempo:       bpm
+  # beat_count:  [ odd_count, container_count] i.e. [5, 32]
+  # repetitions: play the whole thing N times
+  # solo:        either :melody or :rhythm, if only one should be odd
+  def self.odd_time(options={})
+      tempo = options[:tempo]
+      solo = options[:solo]
+    # init odd time counts for entire phrase
+      if options[:beat_counts]
+        sequence = OddTimePhrase.new(*options[:beat_counts])
+      else
+        sequence = OddTimePhrase.random
+      end
+      min = sequence.min
+      max = sequence.max
+      puts "#{"odd time sequence".blue}: #{sequence.odd_time_sequence.join(", ").green}"
+      (options[:repetitions] || 4).times do
+        sequence.odd_time_sequence.each do |beats_count|
+          bloop = BloopApi.new(tempo: tempo || 500)
+          puts tempo || 500
+          bloop.bloops.tune(bloop.bass_drum, BloopApi.odd_rhythm_riff(beats_count)) unless solo == :melody
+          bloop.bloops.tune(bloop.bass_tone, BloopApi.odd_melody_riff(beats_count)) unless solo == :rhythm
+          bloop.bloops.play
+          puts "playing #{beats_count}"
+          sleep 0.1 while !bloop.bloops.stopped?
+        end
+      end 
+
+      # How to play this in parallel?
+      #     # standard time sequence
+      #       bloop = BloopApi.new(tempo: tempo || 500)
+      #       puts tempo || 500
+      #       bloop.bloops.tune(bloop.new_rhythm_sound, BloopApi.standard_rhythm_riff(max)) unless solo == :rhythm
+      #       bloop.bloops.tune(bloop.new_melody_sound, BloopApi.standard_melody_riff(max)) unless solo == :melody
+      #       puts "playing #{max}"
+      #       bloop.bloops.play
+      #       sleep 0.1 while !bloop.bloops.stopped?
+      #   }
+  end
+    
+            #   Parallel.each([1,2]) do |idx|
+            #     sequence.odd_time_sequence.each do |beats_count|
+            #         bloop = self.new(tempo: options[:tempo])
+            #         bloop.tune(new_rhythm_sound, odd_rhythm_riff(sequence.min))
+            #         bloop.tune(new_melody_sound, odd_melody_riff(sequence.min))
+            #         bloop.play
+            #       end
+            #   end
+
+            #   bloops[0].tune(new_rhythm_sound, odd_rhythm_riff(min))
+            #   bloops[0].tune(new_melody_sound, odd_melody_riff(min))
+            #   bloops.each { |bloop| bloop.play }
+            #   sleep 0.5 while (!(bloops.all { |bloop| bloop.stopped? }))
+            # end
+
   def self.flow(options={})
+    # play random riffs
+    # options: length, intensity_max, intensity_min
+    # intensity here referrers to the number of instruments played
+    # a random number in the bounds is used for each phrase
     options = {
       length: 4,
-      intensity_max: 5,
-      intensity_min: 0,
+      intensity_max: 20,
+      intensity_min: 10,
     }.merge(options)
     options[:length].times {
-      self.new.play_melody(
+      self.new.generate_riff(
         intensity: [rand(options[:intensity_max]), options[:intensity_min]].max
       )
     }
   end
 
-  def play_melody(options={})
+  def self.sick_beat(options={})
+    # plays a 'sick beat' with variable intensity
+    # options: length, intensity_min, intensity_max, tempo
+    # intensity here referrers to loudness (track stacking)
+    # a random number in the bounds is used for each phrase
+    options = {
+      length: 4,
+      intensity_min: 2,
+      intensity_max: 3,
+      tempo: 320,
+    }.merge(options)
+    options[:length].times {
+      self.new(tempo: options[:tempo]).play_sick_beat(
+        intensity: [rand(options[:intensity_max]), options[:intensity_min]].max
+      )
+    }
+  end
+
+  def play_sick_beat(options={})
+    # plays a single phrase of 'sick beat'
+    # options: intensity (num of concurrent tracks i.e. loudness)
+    options = {
+      intensity: 1
+    }.merge(options)
+    bass_riff = "- D 4 4 D 4 4 D 4 4 4 D 4 4 4 4 4"
+    snare_riff = "- 4 4 4 4 D 4 4 4 4 4 4 4 D 4 4 4 "
+    hand_riff = "4 D D 4 4 D 4 D D D 4 D 4 D D D"
+    melody_riff = "- - - - 1A 1A 1A 2A 2E"
+    [0, options[:intensity]].max.times {
+      melody = new_melody_sound
+      @bloops.tune(bass_drum, bass_riff)
+      @bloops.tune(snare_drum, snare_riff)
+      @bloops.tune(hand_drum, hand_riff)
+      @bloops.tune(melody, melody_riff)
+      @bloops.play
+    }
+    sleep 0.05 while !@bloops.stopped?
+  end
+
+  def generate_riff(options={})
+    # play a single measure with random instruments and melody
+    # options: intensity (num concurrent tracks)
     options = {
       intensity: 1
     }.merge(options)
@@ -40,44 +277,36 @@ class BloopApi
   end
 
   def new_melody_sound
-    self.send([:base_tone, :chord_tone, :lead_tone].sample)
+    # random sound out of the 'melodies'
+    self.send([:bass_tone, :chord_tone].sample)
   end
 
   def new_rhythm_sound
-    self.send([:bass_drum, :snare_drum].sample)
+    # random sound out of the 'rhythms'
+    self.send([:bass_drum, :snare_drum, :hand_drum].sample)
   end
 
   def rhythms
+    # rhythm phrases
     [
-      "A d6 d6 d6 B d6 d6 d6 A d6 d6 d6 sB d6 d6 d6",
-      "4   c6 4  b5 4  4  e4 4   c6 4  b5 4  4  e4",
-      "4 4a2 4 4a2 4 4a2 4 8a2 8a2",
-      "4a2 4 4 4a2 4 4 4a2 4 4 4a2 4 4 ",
-      "8 4a1 8a 8a 4a 4a 4c2 9c 7c 4c 8e",
-      "8 4g2 8g 8 4g 4g 4d2 8 8c3 4b2 8e",
-      "8 4d2 8c 8c 4c 4 4a2 9a 7a 4a 8a",
-      "8 4g2 8g 8 4g 4d 4d2 9e3 7c2 8 4b1",
+    "A A C A A A C A A A C A A A C A ",
+    "A 4 4 A 4 4 A 4 4 A 4 4 A 4 4 A"
     ]
   end
 
   def melodies
+    # melody phrases
     [
-      " 32 + C E F# 8:A G E C - 8:A 8:F# 8:F# 8:F# 2:G",
-    "f#5 c6 e4 b6 g5 d6 4  f#5 e5 c5 b6 c6 d6 4 ",
-    "1a2 2a3 2 1d2 4g3 4g2 2e3",
-    "1a2 2a3 2 1g2 2d3 2",
-    "2g3 1a4 2 2g3 1a4 2",
-    "2c5 1e4 2 1 2c5 1e4 2 1",
-    "1a4 2 4 2e4 1d4 2",
-    "2a3 1b4 2 2a3 1b4 2",
-    "2d5 2g4 1c5 2d5 2g4 1c5",
-    "1a4 1e5 1a4 1e5",
-    "1b4 2 4 8 8d4 1b4 2 4 8 8d4 1b4 2 4 8 8d4    ",
-
+      "A 4 2 A 4 2 A 4 2 A 4 2",
+      "1 1 1 1 A A A A ",
+      "1A 1F# 1A 1F#",
+      "B C F# 16 D",
+      " C D A B"
     ]
   end
 
-  def bass_drum
+  def bass_tone
+    # melody instrument
     @bloops.sound(Bloops::SQUARE).tap do |bass|
       bass.volume = 0.4
       bass.sustain = 0.1
@@ -86,7 +315,8 @@ class BloopApi
     end
   end
 
-  def base_tone
+  def bass_drum
+    # rhythm instrument    
     @bloops.sound(Bloops::SQUARE).tap do |base|
       base.volume = 0.35
       base.punch = 0.3
@@ -100,6 +330,7 @@ class BloopApi
   end
 
   def snare_drum
+    # rhythm instrument    
     @bloops.sound(Bloops::SQUARE).tap do |snare|
       snare.attack = 0.075
       snare.sustain = 0.01
@@ -111,6 +342,7 @@ class BloopApi
   end
 
   def chord_tone
+    # melody instrument    
     @bloops.sound(Bloops::SQUARE).tap do |chord|
       chord.volume = 0.275
       chord.attack = 0.05
@@ -123,7 +355,8 @@ class BloopApi
     end
   end
 
-  def lead_tone
+  def hand_drum
+    # rhythm instrument    
     @bloops.sound(Bloops::SINE).tap do |lead|
       lead.volume = 0.45
       lead.attack = 0.3
--- cli_commands.rb
+++ cli_commands.rb
@@ -6,11 +6,21 @@ class Commands
     # make sure this method accepts a hash argument, which is passed from OptionParser
     puts "try 'help'".yellow
   end
+  def odd_time(options={})
+    # options: tempo, beat_count, repetitions
+    BloopApi.odd_time(
+      tempo: options[:tempo] || 100,
+      beat_count: options[:beat_count],
+      repetitions: options[:repetitions] || 4,
+      # solo: :rhythm,
+    )
+  end
+
   def play_chiptune
     BloopApi.flow(
-      length: 8,
-      intensity_max: 5,
-      intensity_min: 10
+      length: 16,
+      intensity_min: 10,
+      intensity_max: 18,
     )
   end
   def parallel_chiptune(count)
@@ -21,4 +31,20 @@ class Commands
       puts "your computer cant handle it".red
     end
   end  
+  def sick_beat(count=4)
+    BloopApi.sick_beat(
+      length: count, 
+      intensity_min: 8,
+      intensity_max: 8
+    )
+  end
+  def parallel_sick_beat(count)
+    begin
+      Parallel.each(count.times.to_a) { play_chiptune }
+    rescue StandardError => e
+      puts e.message
+      puts "your computer cant handle it".red
+    end
+  end  
+  
 end
